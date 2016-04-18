737768b8b5f5ba0f7ccb7bae70d86b6947e0c527
hirose yudai <kingkong999yhirose@gmail.com>
2016-04-18 19:18:56 +0900

    InitialCommit

---
+++ SegueAddition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueAddition
+//
+//  Created by kingkong999yhirose on 2016/04/18.
+//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
